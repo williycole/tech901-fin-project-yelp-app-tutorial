906544418695a7302b933bd1c8778d599a1a557e
williycole <64283810+williycole@users.noreply.github.com>
2020-11-30 20:51:08 -0600

    Initial Commit
    Initial Commit

---
+++ src/Components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Business from '../Business/Business';
+import './Business.css';
+
+class BusinessList extends React.Component{
+    render(){
+        return(
+            <div className="BusinessList">
+               <Business />
+               <Business />
+               <Business />
+               <Business />
+               <Business />
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
